281c6561e10e8706efb5b4a2017de4a4acf9ebde
shiguera <shiguera@mercatorlab.com>
2023-12-03 11:13:24 +0100

    Solve struct exercise

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,11 +1,13 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum Lang {
   English,
   Spanish,
   Chinese,
   Texan,
+  French
 }
 
+#[derive(Clone)]
 struct Greeting {
     message: String,
     lang: Lang,
@@ -13,7 +15,6 @@ struct Greeting {
 
 fn main() {
   let mut v :Vec<Greeting> = Vec::new();
-
   let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
@@ -22,8 +23,17 @@ fn main() {
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
   v.push(g);
+  let g : Greeting = Greeting { lang: Lang::French, message: String::from("¡Bonjour, WasmEdge!") };
+  v.push(g);
 
-  for e in v {
-    println!("{:?} {}", e.lang, e.message);
-  }
+  //for e in &v {
+  //   println!("{:?} {}", e.lang, e.message);
+  //}
+
+  let language = Lang::Spanish;
+  let w: Greeting = v.clone().into_iter().find(|g| g.lang==language).unwrap();
+  println!("{} {:?}", w.message, w.lang);
+  let language = Lang::French;
+  let w: Vec<Greeting> = v.clone().into_iter().filter(|g| g.lang==language).collect();
+  println!("{:?} {}", w[0].lang, w[0].message);
 }
